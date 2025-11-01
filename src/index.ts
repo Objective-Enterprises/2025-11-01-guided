4bf9813c6b033ace85439136a396d71c31031d15
David Y. Stephenson <david@davidystephenson.com>
2025-11-01 13:02:34 -0400

    Display characters

--- src/index.ts
+++ src/index.ts
@@ -18,11 +18,27 @@ interface Character {
 }
 
 // Array to store all characters
+const characters: Character[] = []
 
 // Function to add a new character
-
+function addCharacter (character: Character) {
+  characters.push(character)
+}
 
 // Function to display all characters
+function displayCharacters () {
+  for (const character of characters) {
+    const wizard = character.isWizard ? 'wizard' : 'muggle'
+    const spells = character.spells.length === 0
+      ? 'no spells'
+      : `the spells ${character.spells.join(' and ')},`
+    const wand = character.wand[0] === 'None'
+      ? 'no wand'
+      : `whose wand has a core of ${character.wand[0]}, a length of ${character.wand[1]}, and a material of ${character.wand[2]}`
+    const message = `${character.name} is a ${character.age} year old ${wizard} from house ${character.house} with ${spells} and ${wand}.`
+    console.log(message)
+  }
+}
 
 // Function to filter characters by house
 
@@ -30,35 +46,35 @@ interface Character {
 
 
 // Sample data
-// addCharacter({
-//   name: "Harry Potter",
-//   age: 17,
-//   isWizard: true,
-//   house: House.Gryffindor,
-//   spells: ["Expelliarmus", "Expecto Patronum"],
-//   wand: ["Phoenix Feather", 11, "Holly"]
-// });
+addCharacter({
+  name: "Harry Potter",
+  age: 17,
+  isWizard: true,
+  house: House.Gryffindor,
+  spells: ["Expelliarmus", "Expecto Patronum"],
+  wand: ["Phoenix Feather", 11, "Holly"]
+});
 
-// addCharacter({
-//   name: "Hermione Granger",
-//   age: 17,
-//   isWizard: true,
-//   house: House.Gryffindor,
-//   spells: ["Alohomora", "Petrificus Totalus"],
-//   wand: ["Dragon Heartstring", 10.75, "Vine"]
-// });
+addCharacter({
+  name: "Hermione Granger",
+  age: 17,
+  isWizard: true,
+  house: House.Gryffindor,
+  spells: ["Alohomora", "Petrificus Totalus"],
+  wand: ["Dragon Heartstring", 10.75, "Vine"]
+});
 
-// addCharacter({
-//   name: "Dudley Dursley",
-//   age: 18,
-//   isWizard: false,
-//   house: House.Slytherin,
-//   spells: [],
-//   wand: ["None", 0, "None"]
-// });
+addCharacter({
+  name: "Dudley Dursley",
+  age: 18,
+  isWizard: false,
+  house: House.Slytherin,
+  spells: [],
+  wand: ["None", 0, "None"]
+});
 
 // Displaying characters
-// displayCharacters()
+displayCharacters()
 
 // Filter by house
 // console.log("\n Characters from Gryffindor:");
