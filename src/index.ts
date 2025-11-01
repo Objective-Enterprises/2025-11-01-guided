d4f61f32dc8c042f369f5981d5a6db72c10fb577
David Y. Stephenson <david@davidystephenson.com>
2025-11-01 12:44:17 -0400

    Define Character

---
+++ src/index.ts
@@ -0,0 +1,69 @@
+// Type definition for wand using tuple
+type Wand = [core: string, length: number, material: string]
+
+// Enum for house names
+enum House {
+  Gryffindor = 'Gryffindor',
+  Slytherin = 'Slytherin'
+}
+
+// Type for character
+interface Character {
+  name: string
+  age: number
+  isWizard: boolean
+  house: House
+  spells: string[]
+  wand: Wand
+}
+
+// Array to store all characters
+
+// Function to add a new character
+
+
+// Function to display all characters
+
+// Function to filter characters by house
+
+// Function to count wizards vs muggles
+
+
+// Sample data
+// addCharacter({
+//   name: "Harry Potter",
+//   age: 17,
+//   isWizard: true,
+//   house: House.Gryffindor,
+//   spells: ["Expelliarmus", "Expecto Patronum"],
+//   wand: ["Phoenix Feather", 11, "Holly"]
+// });
+
+// addCharacter({
+//   name: "Hermione Granger",
+//   age: 17,
+//   isWizard: true,
+//   house: House.Gryffindor,
+//   spells: ["Alohomora", "Petrificus Totalus"],
+//   wand: ["Dragon Heartstring", 10.75, "Vine"]
+// });
+
+// addCharacter({
+//   name: "Dudley Dursley",
+//   age: 18,
+//   isWizard: false,
+//   house: House.Slytherin,
+//   spells: [],
+//   wand: ["None", 0, "None"]
+// });
+
+// Displaying characters
+// displayCharacters()
+
+// Filter by house
+// console.log("\n Characters from Gryffindor:");
+// console.log(filterByHouse(House.Gryffindor));
+
+// Magical status summary
+// const summary = countByMagicalStatus();
+// console.log(`\n Wizards: ${summary.wizards}, Muggles: ${summary.muggles}`);
